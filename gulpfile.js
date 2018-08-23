20883549171992fc7730f7d1f26f4de6b3912ce0
Woojong Kim <woojong92@gmail.com>
2018-08-23 21:45:11 +0900

    installed babel & gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp=require('gulp'); //걸프 의존성을 여기 씁니다.
+const babel=require('gulp-babel');
+
+
+gulp.task('default', function(){//걸프 작업을 여기 씁니다.
+	//노드 소스
+	gulp.src("es6/**/*.js")
+	 .pipe(babel())
+	 .pipe(gulp.dest("dist"));
+	//브라우저 소스
+	gulp.src("public/es6/**/*.js")
+	 .pipe(babel())
+	 .pipe(gulp.dest("public/dist"));
+});
+
+
